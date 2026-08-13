727d68aec013e9130e24f0d53835ec341987a71a
cichrrad <cichra.r.business@gmail.com>
2026-08-13 18:55:37 +0000

    001 hello world!

---
+++ 001_hello_world/main.c
@@ -0,0 +1,28 @@
+// mem layout from RM0440 
+#define RCC_AHB2ENR (*(volatile unsigned int *)0x4002104C) // Peripheral clock enable reg.
+#define GPIOA_MODER (*(volatile unsigned int *)0x48000000) // Port A pins mode reg.
+#define GPIOA_ODR (*(volatile unsigned int *)0x48000014)   // Port A pins output data reg.
+
+int main(void)
+{
+
+    // Bit 0 enables clock for GPIOA
+    RCC_AHB2ENR |= (1 << 0);
+
+    // Change Port A pin 5 (LED) mode to be:
+    // [01]: General purpose output mode
+    // -> set [01] on pins [11,10]
+    GPIOA_MODER &= ~(3 << 10);
+    GPIOA_MODER |= (1 << 10);
+    
+    while (1)
+    {
+        // Toggle Port A pin 5 (LED) output
+        GPIOA_ODR ^= (1 << 5);
+        for (volatile int i = 0; i < 5000000; i++)
+        {
+            // Zzzzz
+        }
+    }
+    return 0;
+}
